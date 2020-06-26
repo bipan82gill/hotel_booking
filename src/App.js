a33a21649fc107f1d70ff42994508a9a009f3d82
bipan82gill <56560207+bipan82gill@users.noreply.github.com>
2020-06-26 17:49:23 -0400

    adding routes and pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import React from 'react';
 
+import { Route, Switch } from 'react-router-dom';
+
+import Home from './pages/Home';
+import Rooms from './pages/Rooms';
+import SingleRoom from './pages/SingleRoom';
+import Error from './pages/Error';
+
 import './App.css';
 
 function App() {
   return (
+   
     <div className="App">
-      <h1> Welcome to Hotel Booking App</h1>
+    
+      <Switch>
+      <Route exact path='/'  component={Home}/>
+      <Route exact path='/rooms/' component={Rooms}/>
+      <Route exact path='/rooms/:slug' component={SingleRoom}/>
+      <Route component={Error}/>
+     </Switch>
+    
     </div>
+   
   );
 }
 
