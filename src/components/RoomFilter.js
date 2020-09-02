c595d58dab24ef97a543b6796c5cbaf01f10ce2b
bipan82gill <56560207+bipan82gill@users.noreply.github.com>
2020-09-02 18:47:37 -0400

    filter rooms by number of guests

--- src/components/RoomFilter.js
+++ src/components/RoomFilter.js
@@ -28,16 +28,40 @@ export default function RoomFilter({rooms}) {
           {item}
           </option>
   })
+  let people = getUnique(rooms, 'capacity');
+  people = people.map((item, index)=>{
+      return <option key={index} value={item}>
+          {item}
+          </option>
+  })
     return (
         <section className ="filter-container">
            <Title title="search-rooms"/>
            <form className ="filter-form">
               <div className="form-group">
                 <label htmlFor ="type">room type</label>
-                <select name="type" id ="type" value={type} className="form-control" onChange={handleChange}>
+                <select 
+                name="type" 
+                id ="type" 
+                value={type} 
+                className="form-control" 
+                onChange={handleChange}>
                 {types}
                 </select>
             </div> 
+
+            <div className="form-group">
+                <label htmlFor ="capacity">Guests</label>
+                <select 
+                name="capacity" 
+                id ="capacity" 
+                value={capacity} 
+                className="form-control" 
+                onChange={handleChange}>
+                {people}
+                </select>
+            </div> 
+
             </form>
         </section>
     )
