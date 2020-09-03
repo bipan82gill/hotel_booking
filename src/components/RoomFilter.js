8ba7818584ce4950bdfacd7e1a41cb6c74ee4737
bipan82gill <56560207+bipan82gill@users.noreply.github.com>
2020-09-03 16:38:16 -0400

    filter rooms by room size

--- src/components/RoomFilter.js
+++ src/components/RoomFilter.js
@@ -62,9 +62,10 @@ export default function RoomFilter({rooms}) {
                 </select>
             </div> 
             {/* // room price */}
-            <div className= "form-group">
-                <label htmlFor ="price">
-                    room price ${price}
+           
+            <div className="form-group">
+                <label htmlFor="price">
+                    room price ${price} </label>
                     <input 
                      type="range" 
                      name="price"
@@ -74,7 +75,37 @@ export default function RoomFilter({rooms}) {
                      value ={price} 
                      onChange={handleChange} 
                      className ="form-control"/>
-                </label>
+               
+            </div>
+             {/*size */}
+            <div className="form-group">
+                <label htmlFor ="size">room-size</label>
+               <div className ="size-inputs">
+                <input type ="number" name= "minSize" id="size" value={minSize} onChange ={handleChange} className ="size-input"/>
+                <input type ="number" name= "maxSize" id="size" value={maxSize} onChange ={handleChange} className ="size-input"/>
+            </div>
+            </div>
+            {/* //extra */}
+            <div className="form-group">
+                <div className ="single-extra">
+                <input 
+                type="checkbox" 
+                name="breakfast"
+                id="breakfast"
+                checked ={breakfast}
+                onChange={handleChange}/>
+                <label htmlFor ="breakfast">breakfast</label>
+                </div>
+
+                <div className ="single-extra">
+                <input 
+                type="checkbox" 
+                name="pets"
+                id="pets"
+                checked ={pets}
+                onChange={handleChange}/>
+                <label htmlFor ="pets">pets</label>
+                </div>
             </div>
 
             </form>
